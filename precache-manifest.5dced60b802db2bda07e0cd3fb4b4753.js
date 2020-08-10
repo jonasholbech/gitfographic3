461b8b0846a5346f4d439c371a7c045089876419
Jonas Holbech <jh@jonasholbech.dk>
2020-08-10 22:57:34 +0200

    Updates

---
+++ precache-manifest.5dced60b802db2bda07e0cd3fb4b4753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167a15d7ebee81d7a91128ba0e62ce8f",
+    "url": "/gitfographic3/index.html"
+  },
+  {
+    "revision": "f67406ddebd771955f98",
+    "url": "/gitfographic3/static/css/main.390d2702.chunk.css"
+  },
+  {
+    "revision": "259dbe33b0a8c69728b5",
+    "url": "/gitfographic3/static/js/2.19270583.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/gitfographic3/static/js/2.19270583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67406ddebd771955f98",
+    "url": "/gitfographic3/static/js/main.92366f80.chunk.js"
+  },
+  {
+    "revision": "04fcf2920aa208488490",
+    "url": "/gitfographic3/static/js/runtime-main.5352697c.js"
+  }
+]);
