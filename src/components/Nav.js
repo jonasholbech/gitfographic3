2a99fc2375492cf66d477637eb408fce10c03231
Jonas Holbech <jh@jonasholbech.dk>
2020-08-11 10:07:43 +0200

    funky stuff

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,41 +1,62 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import {
+  ArrowRightCircle,
+  ArrowLeftCircle,
+  ArrowUpCircle,
+} from "react-systemuicons";
+
 import { MachineContext } from "../models/MachineProvider";
 
 export default function Nav(props) {
   const [state, send] = useContext(MachineContext);
+  const [levelsOpen, setLevelsOpen] = useState(false);
+  const classes = (levelsOpen ? "open" : "") + " navWrapper";
   return (
-    <nav>
-      <button
-        disabled={!state.context.unlocks.overviewScene}
-        onClick={() => send("overviewScene")}
-      >
-        Overview
-      </button>
-      <button
-        disabled={!state.context.unlocks.gitignoreScene}
-        onClick={() => send("gitignoreScene")}
-      >
-        .gitignore
-      </button>
-      <button
-        disabled={!state.context.unlocks.commitScene}
-        onClick={() => send("commitScene")}
-      >
-        commits
-      </button>
-      <button
-        disabled={!state.nextEvents.includes("prev")}
-        onClick={() => send("prev")}
-      >
-        Previous
-      </button>
-      <button
-        disabled={!state.nextEvents.includes("next")}
-        onClick={() => send("next")}
-      >
-        Next
-      </button>
-    </nav>
+    <div className={classes}>
+      <ol className="levels">
+        <li>
+          <button
+            disabled={!state.context.unlocks.overviewScene}
+            onClick={() => send("overviewScene")}
+          >
+            {state.context.unlocks.overviewScene ? "Overview" : "Locked"}
+          </button>
+        </li>
+        <li>
+          <button
+            disabled={!state.context.unlocks.gitignoreScene}
+            onClick={() => send("gitignoreScene")}
+          >
+            {state.context.unlocks.gitignoreScene ? ".gitignore" : "Locked"}
+          </button>
+        </li>
+        <li>
+          <button
+            disabled={!state.context.unlocks.commitScene}
+            onClick={() => send("commitScene")}
+          >
+            {state.context.unlocks.commitScene ? ".Commits" : "Locked"}
+          </button>
+        </li>
+      </ol>
+      <nav>
+        <button onClick={() => setLevelsOpen(!levelsOpen)}>
+          <ArrowUpCircle className="up" size={50} />
+        </button>
+        <button
+          disabled={!state.nextEvents.includes("prev")}
+          onClick={() => send("prev")}
+        >
+          <ArrowLeftCircle size={50} />
+        </button>
+        <button
+          disabled={!state.nextEvents.includes("next")}
+          onClick={() => send("next")}
+        >
+          <ArrowRightCircle size={50} />
+        </button>
+      </nav>
+    </div>
   );
 }
 //state.nextEvents
