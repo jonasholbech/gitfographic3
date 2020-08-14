aab90ace019561d3f2d539e8bcbf93b9c276bd1d
Jonas Holbech <jh@jonasholbech.dk>
2020-08-14 10:59:13 +0200

    remove unused compoents from introductionscene

--- src/components/svg/scenes/Introduction.js
+++ src/components/svg/scenes/Introduction.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-import TextBox from "../TextBox";
-import AnnotatedArrow from "../AnnotatedArrow";
-import File from "../File";
-import DrawBox from "../DrawBox";
 import GitLogo from "../GitLogo";
 export default function Introduction() {
   return (
