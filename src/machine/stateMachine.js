0a70b57b5cb4859ce3c75b9e5a94e6cbaca46dae
Jonas Holbech <jh@jonasholbech.dk>
2020-08-16 16:28:28 +0200

    spelling error

--- src/machine/stateMachine.js
+++ src/machine/stateMachine.js
@@ -13,7 +13,7 @@ import actions from "./parts/actions";
 import { initialState, unlockStorage } from "./config";
 //TODO: when switching scenes the text is misplaced, add initial setBox to all scenes
 //topBranchTransitions (in scenes) er nok den rigtige vej at gå, lidt for meget repetition her
-//TODO: if the storage.length (ish) does not match the above, staorage is invalid", clear it? merge it?
+//TODO: if the storage.length (ish) does not match the above, storage is invalid", clear it? merge it?
 let storage = localStorage.getItem(unlockStorage);
 if (!storage) {
   localStorage.setItem(
