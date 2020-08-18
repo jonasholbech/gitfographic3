d9cc9c170ac29f169e3de957418e1c4f7e75c890
Jonas Holbech <jh@jonasholbech.dk>
2020-08-18 12:43:26 +0200

    Updates

---
+++ precache-manifest.595c3f4df779c467145f76cf65417729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec7bd86fc96ce75cd0fca36949c623b",
+    "url": "/gitfographic3/index.html"
+  },
+  {
+    "revision": "36ac25b0103af7478bd5",
+    "url": "/gitfographic3/static/css/main.2b304fc4.chunk.css"
+  },
+  {
+    "revision": "795b98ef6692e3fb3959",
+    "url": "/gitfographic3/static/js/2.e1dd10ca.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/gitfographic3/static/js/2.e1dd10ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ac25b0103af7478bd5",
+    "url": "/gitfographic3/static/js/main.e6f8842a.chunk.js"
+  },
+  {
+    "revision": "b969a228afc2705391c7",
+    "url": "/gitfographic3/static/js/runtime-main.383499d0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gitfographic3/precache-manifest.5f85339ff524013a7d451dd68a0c7636.js"
+  "/gitfographic3/precache-manifest.595c3f4df779c467145f76cf65417729.js"
 );
 
 self.addEventListener('message', (event) => {
